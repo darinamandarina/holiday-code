3c58c1840042ac801b4d671ad8ef4cc9eff42f60
Darina <darinamandarina@users.noreply.github.com>
2017-08-08 17:12:19 +0300

    changes

---
+++ MEAN app/credenials.js
@@ -0,0 +1,8 @@
+mongo: {
+    development: {
+        connectionString: 'mongodb://<dbuser>:<dbpassword>@ds135029.mlab.com:35029/portfolio-db'
+    },
+    production: {
+        connectionString: 'mongodb://<dbuser>:<dbpassword>@ds135029.mlab.com:35029/portfolio-db'
+    },
+}
--- MEAN app/site.js
+++ MEAN app/site.js
@@ -1,11 +1,18 @@
-//let globalPath = '/usr/lib/node_modules/';
-const express = require('express');
-const app = express();
-const handlebars = require('express-handlebars').create({
-    defaultLayout: 'main'
+//var globalPath = '/usr/lib/node_modules/';
+var express = require( 'express');
+var app = express();
+var handlebars = require('express-handlebars').create({
+    defaultLayout: 'main', //настройка шаблона представлений
+    helpers:{
+        section: function(name, options){
+            if(!this._sections) this._sections={};
+            this._sections[name] = options.fn(this);
+            return null;
+        } //настройка вспомогательного элемента #section, для внедрения части кода в представление
+    }
 });
-const bodyParser = require('body-parser');
-const prediction = require('./lib/prediction.js');
+var bodyParser = require('body-parser');
+var prediction = require('./lib/prediction.js');
 exports.start = function () {
     //настаиваем view в MVC
     app.engine('handlebars', handlebars.engine);
@@ -14,13 +21,18 @@ exports.start = function () {
     //порядок важен!
     //первым делом, задаем порт
     app.set('port', process.env.PORT || 3000);
-
+    // активирум  кэширование представлений("вьюшек")
+    app.set('view cache', true);
     //добавим промежуточное ПО (middleware)
     app.use(express.static(__dirname + '/public/'));
     app.use(bodyParser.urlencoded({
         extended: false
     }));
-
+    app.use(function (req, res, next) {
+        if(!res.locals.partials) res.locals.partials = {};
+        res.locals.partials.predictionContext = prediction.getFortune();
+        next();
+    });
     //промежуточное ПО для прогонки тестов и чтения запроса в url после знака ?
     app.use(function (req, res, next) {
         res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
@@ -30,14 +42,15 @@ exports.start = function () {
     //следом за ним идет роутинг (маршрутизация)
     app.get('/', function (req, res) {
         res.render('home', {
-                prediction: prediction.getFortune()});
+            prediction: prediction.getFortune()
+        });
     });
-    //скачивание любого файла с папки проекта при адресации (нажатии на ссылку ссылвющуюся) на указанный файл <a href="/download/filename"></a>
+    /*скачивание любого файла с папки проекта при адресации (нажатии на ссылку ссылвющуюся) на указанный файл <a href="/download/filename"></a>
     app.get('/download/:file', function (req, res) {
-        let file = req.params.file;
-            console.log(file);
+        var file = req.params.file;
+        console.log(file);
         res.download('public/uploads/' + file);
-    });
+    }); */
 
 
     app.get('/about', function (req, res) {
@@ -51,19 +64,19 @@ exports.start = function () {
                 pageTestScript: '/qa/tests-about.js'
             });
         })*/
-        app.get('/reqres', function (req, res) {
+    app.get('/reqres', function (req, res) {
 
-            let request = {
-                Request_path: req.path,
-                Request_IP: req.ip,
-                Request_HOST: req.hostname,
-                Request_protocol: req.protocol,
-                Request_acceptedLanguage: req.acceptedLanguages,
-                Request_url: req.url,
+        var request = {
+            Request_path: req.path,
+            Request_IP: req.ip,
+            Request_HOST: req.hostname,
+            Request_protocol: req.protocol,
+            Request_acceptedLanguage: req.acceptedLanguages,
+            Request_url: req.url,
 
-            };
-            res.send('200', request);
-        });
+        };
+        res.send('200', request);
+    });
 
     app.get('/contact', function (req, res) {
         res.render('contact');
@@ -71,6 +84,7 @@ exports.start = function () {
 
     app.get('/blog', function (req, res) {
         res.render('blog');
+        //res.render('blog', { layout: 'blogLayout' }); - указание макета blogLayout.handlebars для предстваления blog.handlebars
     });
     //зададим страницы отображения для отображения страниц со статусом ответа, отличным от 200
     app.use(function (req, res) {
