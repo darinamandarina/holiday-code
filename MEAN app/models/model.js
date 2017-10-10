2ece9d6f7a223c5fef619cf485e48348114e6195
Darina <darinamandarina@users.noreply.github.com>
2017-10-10 12:41:03 +0300

    merged

--- MEAN app/models/model.js
+++ MEAN app/models/model.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var mongoose = require('mongoose');
 //создадим схему и объявим ее свойства
 var goodsSchema = mongoose.Schema({  
@@ -14,21 +13,4 @@ goodsSchema.methods.getUSprice = function(){
 
 //создадим модель схемы
 var List = mongoose.model('List', goodsSchema);
-=======
-var mongoose = require('mongoose');
-//создадим схему и объявим ее свойства
-var goodsSchema = mongoose.Schema({  
-    name:String,
-    description: String,
-    priceRU:Number,
-    availableNow:Boolean,
-    tags:[String], //массив строк
-});
-goodsSchema.methods.getUSprice = function(){
-    return '$' + (this.priceRU / 60).toFixed(2);
-};
-
-//создадим модель схемы
-var List = mongoose.model('List', goodsSchema);
->>>>>>> 3c58c1840042ac801b4d671ad8ef4cc9eff42f60
 module.exports = List;
