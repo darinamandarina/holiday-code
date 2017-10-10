8cf479162f31cf5fcd83fba31fd77b3d6c017d97
Darina <darinamandarina@users.noreply.github.com>
2017-10-10 12:39:47 +0300

    merged

--- MEAN app/credenials.js
+++ MEAN app/credenials.js
@@ -6,13 +6,4 @@ mongo: {
     production: {
         connectionString: 'mongodb://<dbuser>:<dbpassword>@ds135029.mlab.com:35029/portfolio-db'
     },
-=======
-mongo: {
-    development: {
-        connectionString: 'mongodb://<dbuser>:<dbpassword>@ds135029.mlab.com:35029/portfolio-db'
-    },
-    production: {
-        connectionString: 'mongodb://<dbuser>:<dbpassword>@ds135029.mlab.com:35029/portfolio-db'
-    },
->>>>>>> 3c58c1840042ac801b4d671ad8ef4cc9eff42f60
 }
