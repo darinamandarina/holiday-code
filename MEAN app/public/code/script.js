a0b7976b34809ca7ba12443e97039f836f894062
Darina <darinamandarina@users.noreply.github.com>
2017-10-10 12:41:48 +0300

    merged

--- MEAN app/public/code/script.js
+++ MEAN app/public/code/script.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 $(document).ready(function () {
     $('#btnPrediction').click(function () {
         $('#modalPrediction').modal('show');
     });
-=======
-$(document).ready(function () {
-    $('#btnPrediction').click(function () {
-        $('#modalPrediction').modal('show');
-    });
->>>>>>> 3c58c1840042ac801b4d671ad8ef4cc9eff42f60
 });
